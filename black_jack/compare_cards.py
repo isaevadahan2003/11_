63dd09ae12f52b845b7554a02025e7bf15a11bec
Пользователь <adahamisaev2003@gmail.com>
2021-11-11 18:56:28 +0600

    commited

---
+++ black_jack/compare_cards.py
@@ -0,0 +1,4 @@
+class Compare:
+    def __init__(self, player_list, computer_list):
+        self.player_list = player_list
+        self.computer_list = computer_list
---
+++ python.12.4.py
@@ -0,0 +1,3 @@
+# Написать view которое будет возвращать в браузер  рандомное число от 1 до 10
+# Доп материал:
+# https://docs.djangoproject.com/en/3.2/topics/http/views/
